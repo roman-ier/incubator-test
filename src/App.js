474f351a72a4c8e8de10ae14fa37d4f2d983e44c
ier <r05k@mail.ru>
2021-02-14 04:22:08 +0300

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Search from "./comp/Search";
+import SuperSum from "./comp/SuperSum";
+import ArrMax from "./comp/ArrMax";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            Задачи №1-5:
+            <Search/>
+            ===================<br/>
+            Задача №6:
+            <SuperSum/>
+            ==================<br/>
+            Задачи №7,9:
+            <ArrMax/>
+        </div>
+    );
 }
 
 export default App;
